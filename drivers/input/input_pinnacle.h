130c511bdd79f541c73cc3d165074f6f31191651
Goyakey <9voltppm@gmail.com>
2025-11-29 02:46:35 +0900

    Update input_pinnacle.h

--- drivers/input/input_pinnacle.h
+++ drivers/input/input_pinnacle.h
@@ -9,6 +9,7 @@
 
 #define PINNACLE_AUTOINC 0xFC
 #define PINNACLE_FILLER 0xFB
+#define PINNACLE_WAKE_FILLER 0xF7
 
 // Registers
 #define PINNACLE_FW_ID 0x00   // ASIC ID.
@@ -17,10 +18,19 @@
 #define PINNACLE_STATUS1_SW_DR BIT(2)
 #define PINNACLE_STATUS1_SW_CC BIT(3)
 #define PINNACLE_SYS_CFG 0x03 // Contains system operation and configuration bits.
+
+#define PINNACLE_SYS_CFG_RESET_BIT 0
+#define PINNACLE_SYS_CFG_SHUTDOWN_BIT 1
 #define PINNACLE_SYS_CFG_EN_SLEEP_BIT 2
-#define PINNACLE_SYS_CFG_EN_SLEEP BIT(2)
-#define PINNACLE_SYS_CFG_SHUTDOWN BIT(1)
-#define PINNACLE_SYS_CFG_RESET BIT(0)
+#define PINNACLE_SYS_CFG_WAKE_UP_TOGGLE_BIT 6
+#define PINNACLE_SYS_CFG_FORCE_WAKE_UP_BIT 7
+
+#define PINNACLE_SYS_CFG_RESET BIT(PINNACLE_SYS_CFG_RESET_BIT)
+#define PINNACLE_SYS_CFG_SHUTDOWN BIT(PINNACLE_SYS_CFG_SHUTDOWN_BIT)
+#define PINNACLE_SYS_CFG_EN_SLEEP BIT(PINNACLE_SYS_CFG_EN_SLEEP_BIT)
+
+#define PINNACLE_SYS_CFG_WAKE_UP_TOGGLE BIT(PINNACLE_SYS_CFG_WAKE_UP_TOGGLE_BIT)
+#define PINNACLE_SYS_CFG_FORCE_WAKE_UP BIT(PINNACLE_SYS_CFG_FORCE_WAKE_UP_BIT)
 
 #define PINNACLE_FEED_CFG1 0x04 // Contains feed operation and configuration bits.
 #define PINNACLE_FEED_CFG1_EN_FEED BIT(0)
@@ -56,15 +66,6 @@
 #define PINNACLE_SLEEP_TIMER 0x0D           // Sleep Timer
 #define PINNACLE_AG_PACKET0 0x10            // trackpad Data (Pinnacle AG)
 #define PINNACLE_2_2_PACKET0 0x12           // trackpad Data
-#define PINNACLE_CAL_CFG 0x07                 // Contains calibration configuration bits.
-#define PINNACLE_PS2_AUX 0x08                 // Contains Data register for PS/2 Aux Control.
-#define PINNACLE_SAMPLE 0x09                  // Sample Rate Number of samples generated per second.
-#define PINNACLE_Z_IDLE 0x0A         // Number of Z=0 packets sent when Z goes from >0 to 0.
-#define PINNACLE_Z_SCALER 0x0B       // Contains the pen Z_On threshold.
-#define PINNACLE_SLEEP_INTERVAL 0x0C // Sleep Interval
-#define PINNACLE_SLEEP_TIMER 0x0D    // Sleep Timer
-#define PINNACLE_AG_PACKET0 0x10     // trackpad Data (Pinnacle AG)
-#define PINNACLE_2_2_PACKET0 0x12    // trackpad Data
 #define PINNACLE_REG_COUNT 0x18
 
 #define PINNACLE_REG_ERA_VALUE 0x1B
@@ -93,6 +94,7 @@
 struct pinnacle_data {
     uint8_t btn_cache;
     bool in_int;
+    bool sleep_enabled;
     const struct device *dev;
     struct gpio_callback gpio_cb;
     struct k_work work;
